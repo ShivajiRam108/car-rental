bb71608bd01a7beec822b229d1de785270eb2df4
ShivajiRam108-github <ramudus684@gmail.com>
2025-08-23 18:48:15 +0530

    resolve dashboard issue

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Layout from '../components/layout/Layout';
 import DashboardStats from '../components/dashboard/DashboardStats';
 
 const Dashboard = () => {
   return (
-    <Layout>
+   
       <DashboardStats />
-    </Layout>
+    
   );
 };
 
